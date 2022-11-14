cb99a2496a77fb91576bc82a58c6953fb945a34d
JosephLaybourn <118223246+JosephLaybourn@users.noreply.github.com>
2022-11-14 13:43:25 -0800

    Add files via upload

---
+++ palindrome2.c
@@ -0,0 +1,40 @@
+/*******************************************************************************
+filename    palindrome2.c
+author      Joey Laybourn
+DP email    j.laybourn@digipen.edu
+course      CS120
+section     A
+assignment  3
+due date    10/24/2017
+
+Brief Description:
+Given an array of characters, determine if it's a palindrome using pointers.
+Returns 1 (true) if the phrase is a palindrome and 0 if not.
+*******************************************************************************/
+
+/******************************************************************************
+       Function: is_palindrome2
+
+    Description: Determines if a word is a palendrome or not
+
+         Inputs: phrase - the pointer of the array being tested
+                 length - length of the pointer
+
+        Outputs: 1 or 0 depending on whether the given word 
+                 is a palindrome or not
+******************************************************************************/
+int is_palindrome2(const char *phrase, int length)
+{
+  int i;                                                      /*loop variable*/
+
+  for (i = 0; i < length / 2; i++)  /*loops half the times as the word length*/
+  {
+    char start = *(phrase + i);            /*first letter going to the middle*/
+    char end = *(phrase + (length - i - 1));/*last letter going to the middle*/
+    if (start == end)     /*if letters are the same, try the next two letters*/
+      continue;
+    else                  /*if not, letters are not a palindrome so return 0 */
+      return 0;
+  }
+  return 1;         /*if the loop ends, the word is a palindrome, so return 1*/
+}
